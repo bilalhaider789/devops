c4b32307a1f88f833346e930e0a551b3d38c0ec8
Bilal <bilalhaider1311@gmail.com>
2022-11-01 15:21:27 +0500

    firstcommit

--- controllers/settings.js
+++ controllers/settings.js
@@ -3,5 +3,5 @@ module.exports = {
     // Connect to MonggoDb, 
     // example atlas : mongodb+srv://zekaistodo:password@clusters.rqvdo.mongodb.net/Database?retryWrites=true&w=majority
     // example local : mongodb://127.0.0.1:27017
-    dbURI: 'mongodb+srv://xxx' 
+    dbURI: 'mongodb+srv://bilal:bilal123@cluster0.ddoxwjd.mongodb.net/?retryWrites=true&w=majority' 
 }
